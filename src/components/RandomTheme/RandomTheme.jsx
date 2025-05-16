9de5d1dc13b32fdf1249a59d9dd3feec483de582
Antonino Morana <amorana1508@gmail.com>
2025-05-16 15:54:12 +0000

    Actualizaciòn RandomTheme

--- src/components/RandomTheme/RandomTheme.jsx
+++ src/components/RandomTheme/RandomTheme.jsx
@@ -1,48 +1,44 @@
 import React, { useState, useEffect } from 'react';
+import './RandomTheme.css';
+
+const images = [
+'/imagenes/cappadocia.jpg',
+'/imagenes/matterhorn.jpg',
+'/imagenes/nihn.jpg',
+'/imagenes/pai.jpg',
+'/imagenes/paris.jpg',
+'/imagenes/playasiciliana.jpg',
+'/imagenes/santorini.jpg',
+'/imagenes/scotland.jpg',
+'/imagenes/tajmahal.jpg',
+'/imagenes/whiteheavenbeach.jpg'
+];
 
 const RandomTheme = () => {
-  const images = [
-    '/imagenes/matterhorn.jpg',
-    '/imagenes/playasiciliana.jpg',
-    '/imagenes/whitehavenbeach.jpg',
-    '/imagenes/tajmahal.jpg'
-  ];
-  
   const [bgImage, setBgImage] = useState('');
 
   const handleImageChange = () => {
-    const index = Math.floor(Math.random() * images.length);
-    setBgImage(images[index]);
+    const randomIndex = Math.floor(Math.random() * images.length);
+    setBgImage(images[randomIndex]);
   };
 
   useEffect(() => {
+    handleImageChange();
+  }, []);
+
+  useEffect(() => {
     if (bgImage) {
+      document.body.className = 'background';
       document.body.style.backgroundImage = `url(${bgImage})`;
-      document.body.style.backgroundSize = 'cover';
-      document.body.style.backgroundPosition = 'center';
-      document.body.style.backgroundRepeat = 'no-repeat';
     }
-  
-    
     return () => {
       document.body.style.backgroundImage = '';
-    }
+    };
   }, [bgImage]);
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', margin: '1rem 0' }}>
-      <button 
-        style={{
-          padding: '10px 20px',
-          fontSize: '16px',
-          cursor: 'pointer',
-          backgroundColor: 'rgba(51,51,51,0.7)',
-          color: '#fff',
-          border: 'none',
-          borderRadius: '5px'
-        }} 
-        onClick={handleImageChange}
-      >
+    <div className="button-container" style={{ textAlign: 'center', margin: '20px 0' }}>
+      <button className="theme-button" onClick={handleImageChange}>
         RANDOM THEME
       </button>
     </div>
